d1a17aeb68e88cea6aaadff1c385f7fa21a88e59
"Ademola" <"baruwaademola@outlook.com">
2022-07-24 14:18:20 +0100

    Need to come back to lesson 3

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -2,8 +2,8 @@
 
 pragma solidity >= 0.6.6 < 0.9.0;
 
-import "@chainlink/contacts/src/v0.6/interfaces/AggregatorV3Interface.sol";
-import "@chainlink/contracts/srs/v0.6/vendor/SafeMathChainlink.sol"
+import "@contacts/artifacts/AggregatorV3Interface.sol";
+import "@chainlink/contracts/srs/v0.6/vendor/SafeMathChainlink.sol";
 
 contract FundMe {
     using SafeMathChainlink for uint256;
@@ -14,25 +14,25 @@ contract FundMe {
     // Msg.value is the amount sent.
     // Both required in every contract
     function fund() public payable {
+        uint256 miniimumUSD = 50 * 10 * 18;
+        addressToAmountFunded[msg.sender] += msg.value >= miniimumUSD, "You need to spend more Eth");
         addressToAmountFunded[msg.sender] += msg.value;
     }
 
     function getVersion() public view returns (uint256){
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419);
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
         return priceFeed.version();
     }
 
     function getPrice() public view returns(uint256){
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419);
-        (
-            ,int256 answer,,,
-        )=priceFeed.latestRoundData();
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+        (,int256 answer,,,) = priceFeed.latestRoundData();
         return uint256(answer);
     }
 
     function getConversionRate(uint256 ethAmount) public view returns (uint256){
         uint256 ethPrice = getPrice();
-        uint256 ethAmountInUSD = (ethPrice* ethAmount);
+        uint256 ethAmountInUSD = (ethPrice * ethAmount);
         return ethAmountInUSD;
     }
 }
